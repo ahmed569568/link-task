d021f019ee3e120efaa974551575e6e72f645f16
Ahmed Ezzat <ahmed569568@gmail.com>
2021-07-20 22:48:24 +0200

    close side menu on router change

--- src/app/@shared/burger-button/burger-button.component.ts
+++ src/app/@shared/burger-button/burger-button.component.ts
@@ -1,15 +1,20 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { LayoutService } from '@app/@shared/services/layout.service';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-burger-button',
   templateUrl: './burger-button.component.html',
   styleUrls: ['./burger-button.component.scss'],
 })
-export class BurgerButtonComponent {
+export class BurgerButtonComponent implements OnInit {
   menuStatus: boolean = false;
   @Output() toggle: EventEmitter<any> = new EventEmitter();
 
-  constructor() {}
+  constructor(private layoutService: LayoutService) {}
+
+  ngOnInit() {
+    this.layoutService.menuSubject.subscribe((status: boolean) => (this.menuStatus = status));
+  }
 
   toggleButton(status: boolean) {
     this.menuStatus = !this.menuStatus;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { LayoutService } from '@app/@shared/services/layout.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
@@ -23,6 +24,7 @@ export class AppComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private titleService: Title,
     private translateService: TranslateService,
+    private layoutService: LayoutService,
     // do not remove the analytics injection, even if the call in ngOnInit() is removed
     // this injection initializes page tracking through the router
     private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
@@ -43,7 +45,10 @@ export class AppComponent implements OnInit, OnDestroy {
     // Setup translations
     this.i18nService.init(environment.defaultLanguage, environment.supportedLanguages);
 
-    const onNavigationEnd = this.router.events.pipe(filter((event) => event instanceof NavigationEnd));
+    const onNavigationEnd = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.layoutService.toggleMenu(false))
+    );
 
     // Change page title on navigation or language change, based on route data
     merge(this.translateService.onLangChange, onNavigationEnd)
